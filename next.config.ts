bde130a7993919b6892f075c9340169b2fbe84a7
Drive-by-Si <154678590+Drive-by-Si@users.noreply.github.com>
2026-01-29 22:51:56 +0000

    Fix formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   }
 };
 
